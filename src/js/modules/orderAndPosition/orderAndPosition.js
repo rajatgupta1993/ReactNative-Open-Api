4cdd9d7b85d9f732103b8f0784be37037cc1ba30
Rajat Gupta <rajatgupta.nsit@gmail.com>
2017-11-14 17:21:45 +0530

    orders done with ui

--- src/js/modules/orderAndPosition/orderAndPosition.js
+++ src/js/modules/orderAndPosition/orderAndPosition.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Container, Header, Content, Tab, Tabs } from 'native-base';
 import OrdersTab from './ordersTab';
 import PositionTab from './positionTab';
+import * as queries from './queries';
+import Loader from './../loader/loader';
 
 export default class OrderAndPosition extends Component {
 
@@ -9,21 +11,38 @@ export default class OrderAndPosition extends Component {
         super(props);
            console.log("props in order&position",props);
         this.state = {
-            selectedAccount: props.selectedAccount,
+            selectedAccount: null,    
         };
+        this.tradeTypeId = "OrderId";
+        this.handleAccountSelect=this.handleAccountSelect.bind(this);
     }
+
+    componentDidMount() {
+        queries.fetchAccountInfo(this.props, (response) => {
+            this.setState({ accounts: queries.getAccountArray(response)});
+            this.handleAccountSelect(this.state.accounts[0]);
+        });
+      
+    }
+
+       handleAccountSelect(account) {
+        this.setState({ selectedAccount: account });
+    }
+
 
     componentWillReceiveProps(nextProps) {
         this.setState({ selectedAccount: nextProps.selectedAccount });
     }
   render() {
+    console.log("states in O&P",this.state);
+    const {selectedAccount, accounts} =this.state;
     return (
-      <Container>
+     (accounts && accounts.length>0)? (<Container>
        
         <Tabs initialPage={0}>
           <Tab heading="Orders">
             <OrdersTab  {...this.props}
-                        currentAccountInformation={this.state.selectedAccount}
+                        currentAccountInformation={accounts[0]}
                         tradeType="Order"
                         fieldGroups={['DisplayAndFormat', 'ExchangeInfo']}/>
           </Tab>
@@ -31,7 +50,7 @@ export default class OrderAndPosition extends Component {
             <PositionTab />
           </Tab>
         </Tabs>
-      </Container>
+      </Container>) : null
     );
   }
 }
