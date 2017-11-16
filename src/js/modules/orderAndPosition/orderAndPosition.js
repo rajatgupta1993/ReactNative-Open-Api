951ead1216bfddb63cd9dca3c10f4b779d0d33fc
Gaurav Gera (GAUG) <gaug@saxobank.com>
2017-11-16 18:28:36 +0530

    positions tab

--- src/js/modules/orderAndPosition/orderAndPosition.js
+++ src/js/modules/orderAndPosition/orderAndPosition.js
@@ -47,7 +47,18 @@ export default class OrderAndPosition extends Component {
                         fieldGroups={['DisplayAndFormat', 'ExchangeInfo']}/>
           </Tab>
           <Tab heading="Positions">
-            <PositionTab />
+            <PositionTab {...this.props}
+                        currentAccountInformation={accounts[0]}
+                        tradeType="NetPosition"
+                        fieldGroups={['NetPositionView',
+                            'NetPositionBase',
+                            'DisplayAndFormat',
+                            'ExchangeInfo',
+                            'SingleAndClosedPositionsBase',
+                            'SingleAndClosedPositionsView',
+                            'SingleAndClosedPositions'
+                        ]}
+            />
           </Tab>
         </Tabs>
       </Container>) : null
